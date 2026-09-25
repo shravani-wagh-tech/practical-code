aa2c54f23f53beb69e9959d9385523830210d2b9
shravani-wagh-tech <shravaniw672@gmail.com>
2026-09-25 11:27:30 +0530

    Add files via upload

---
+++ p6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter two no:");
+    scanf("%d%d",&a,&b);
+    printf("addition = %d\n",a+b);
+    printf("sub = %d\n",a-b);
+    printf("mul = %d\n", a*b);
+    return 0;
+}
